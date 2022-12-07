af4b7c86479b1902c914d6052e67c5b949991648
Mike Farrow <mike@farrow.io>
2022-12-07 12:06:00 +0000

    fixing wikipedia title extract

--- index.js
+++ index.js
@@ -409,7 +409,7 @@ async function run() {
     if (mbData.wikidata && !mbData.wikipedia)
       wikidata = await getWikidata(mbData.wikidata);
 
-    const title = wikidata?.title || mbData.wikipedia?.split(/\//).pop();
+    const title = wikidata?.title || mbData.wikipedia?.split(/wiki\//).pop();
 
     if (title) {
       wikipediaData = await getWikipediaData(title);
