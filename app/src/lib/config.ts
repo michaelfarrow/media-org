2c042d3fbb5584c23034b51f17336e500f4403d8
Mike Farrow <mike@farrow.io>
2024-02-27 09:51:40 +0000

    updating paths

--- app/src/lib/config.ts
+++ app/src/lib/config.ts
@@ -1,8 +1,8 @@
 export const MEDIA_DIR = '/media';
 export const MUSIC_DIR = `${MEDIA_DIR}/music`;
-export const MUSIC_LOSSESS_DIR = `${MUSIC_DIR}/lossless`;
+export const MUSIC_LOSSESS_DIR = `${MUSIC_DIR}/lossless_old`;
 export const MUSIC_LOSSESS_NEW_DIR = `${MUSIC_DIR}/lossless_new`;
-export const MUSIC_LOSSESS_FINAL_DIR = `${MUSIC_DIR}/lossless_final`;
+export const MUSIC_LOSSESS_FINAL_DIR = `${MUSIC_DIR}/lossless`;
 export const MUSIC_COMPRESSED_DIR = `${MUSIC_DIR}/compressed`;
 export const MUSIC_NEW_DIR = `${MUSIC_DIR}/__new`;
 
