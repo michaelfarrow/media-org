956387aa8bb07ec31674dc1887de72d1607d75a4
Mike Farrow <mike@farrow.io>
2024-02-28 13:28:44 +0000

    addyears util

--- app/src/addyears.ts
+++ app/src/addyears.ts
@@ -2,7 +2,12 @@ import path from 'path';
 import fs from 'fs-extra';
 import { getDirs } from '@/lib/fs';
 import { MUSIC_LOSSESS_FINAL_DIR, RELEASE_FILE } from './lib/config';
-import { Release, releasePath } from './lib/namer';
+import {
+  Release,
+  releasePath,
+  trackFileName,
+  getReleaseFiles,
+} from './lib/namer';
 
 async function main() {
   const artists = await getDirs(MUSIC_LOSSESS_FINAL_DIR);
@@ -16,11 +21,39 @@ async function main() {
         throw new Error(`Release file does not exist: ${releaseFile}`);
 
       const release = Release.parse(await fs.readJson(releaseFile));
-      const dest = path.resolve(MUSIC_LOSSESS_FINAL_DIR, releasePath(release));
+      const releaseDest = path.resolve(
+        MUSIC_LOSSESS_FINAL_DIR,
+        releasePath(release)
+      );
 
-      if (src !== dest) {
-        console.log('Renaming', src, '>', dest);
-        fs.rename(src, dest);
+      if (src !== releaseDest) {
+        console.log('Renaming', src, '>', releaseDest);
+        fs.rename(src, releaseDest);
+      }
+
+      const discs = release.discs;
+      const files = (await getReleaseFiles(src, ['m4a'])) || [];
+
+      for (var discNumber = 0; discNumber < discs.length; discNumber++) {
+        const disc = discs[discNumber];
+
+        for (var trackNumber = 0; trackNumber < disc.length; trackNumber++) {
+          const file = files[discNumber]?.files[trackNumber];
+
+          if (!file) {
+            throw new Error(`Cannot find file ${discNumber}${trackNumber}`);
+          }
+
+          const trackDest = path.resolve(
+            releaseDest,
+            `${trackFileName(release, discNumber, trackNumber)}.m4a`
+          );
+
+          if (file.path !== trackDest) {
+            console.log('Renaming', file.path, '>', trackDest);
+            fs.rename(file.path, trackDest);
+          }
+        }
       }
     }
   }
