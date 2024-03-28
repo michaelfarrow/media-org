b80f6a2c32be7ad0010a36cd9ab0f6ba5591fddc
Mike Farrow <mike@farrow.io>
2024-03-28 20:47:48 +0000

    account for original removal

--- app/src/movies/workflows/compress.ts
+++ app/src/movies/workflows/compress.ts
@@ -7,13 +7,16 @@ import { MOVIE_AUDIO_TYPES } from '@/lib/config';
 import { processMovies } from './shared/process-movies';
 
 export default async function compress(src: string) {
-  await processMovies(src, async ({ file, streams }) => {
+  await processMovies(src, async ({ file, data, streams }) => {
     if (
       streams.video.width &&
       streams.video.width >= 1280 &&
       MOVIE_AUDIO_TYPES.includes(streams.audio.codec_name || '')
     ) {
-      if (!file.nameWithoutExt.match(/\.original$/)) {
+      if (
+        !file.nameWithoutExt.match(/\.original$/) &&
+        data.format.tags?.COMPRESSED !== 'yes'
+      ) {
         const originalPath = path.resolve(
           file.dir,
           `${file.nameWithoutExt}.original.${file.ext}`
@@ -30,9 +33,11 @@ export default async function compress(src: string) {
           ['-c:s', 'copy'],
           ['-c:v', 'libx264'],
           ['-crf', '16'],
-          ['-profile', 'medium'],
+          ['-preset', 'medium'],
           ['-vf', 'scale=1280:-2'],
           ['-g', '30'], // Set i-frames every 30 frames to speed up scrubbing
+          ['-t', '00:00:10'],
+          ['-metadata', 'COMPRESSED=yes'],
         ]);
 
         await fs.rename(file.path, originalPath);
